3a6700d3c3041ba93c98a836bbd4b744a4a029c9
mengchengma <mengchengma@gmail.com>
2025-10-20 10:18:54 -0400

    Finished Solver.py

--- src/static/script.js
+++ src/static/script.js
@@ -1,6 +1,6 @@
 // Initialize default boards
 let startBoard = [[7, 2, 4], [5, 0, 6], [8, 3, 1]];
-let goalBoard = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+let goalBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 0]];
 
 // Render puzzle on page load
 window.onload = function() {
